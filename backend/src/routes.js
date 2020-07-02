aa6aa89fa5b931a04813183f66a0ce115e5f7e74
indominuShot <luang193@gmail.com>
2020-07-01 21:05:12 -0300

    Rotas atualizadas

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -5,5 +5,7 @@ const routes = express.Router();
 const StudentController = require('./Controllers/StudentController');
 
 routes.get('/students', StudentController.index);
+routes.post('/students/register', StudentController.create);
+routes.post('/students/login', StudentController.login);
 
 module.exports = routes;
